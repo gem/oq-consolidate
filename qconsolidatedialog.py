5ddac49de919d1d808b7a36d0e0d43a02e3be930
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2018-03-21 09:54:26 +0100

    Failed attempt to use QgsTask.fromFunction

--- qconsolidatedialog.py
+++ qconsolidatedialog.py
@@ -22,14 +22,22 @@
 
 
 from builtins import str
+from shutil import copyfile
+from osgeo import gdal
 import os
 import re
-
+import zipfile
 from qgis.PyQt.QtCore import (
                               QDir,
                               QFile,
+                              QIODevice,
+                              QTextStream,
                               QFileInfo,
+                              pyqtSignal,
+                              pyqtSlot,
+                              QMutex,
                               )
+from qgis.PyQt.QtXml import QDomDocument
 from qgis.PyQt.QtWidgets import (
                                  QCheckBox,
                                  QDialog,
@@ -42,15 +50,21 @@ from qgis.PyQt.QtWidgets import (
                                  QHBoxLayout,
                                  QVBoxLayout,
                                  )
-
-from qgis.core import QgsProject, QgsApplication, QgsTask
+from qgis.core import (
+                       QgsMapLayer,
+                       QgsVectorFileWriter,
+                       QgsProject,
+                       QgsTask,
+                       QgsApplication,
+                       )
 from qgis.utils import iface
-
-from .consolidatethread import ConsolidateTask
 from .utils import log_msg, tr
 
 
 class QConsolidateDialog(QDialog):
+
+    outfileCreated = pyqtSignal(str)
+
     def __init__(self):
         QDialog.__init__(self)
         self.initGui()
@@ -74,6 +88,8 @@ class QConsolidateDialog(QDialog):
             self.project_name_le.setText(project_name)
 
         self.btnBrowse.clicked.connect(self.setOutDirectory)
+        self.outfileCreated.connect(self.on_outfileCreated)
+        self.mutex = QMutex()
 
     def initGui(self):
         self.setWindowTitle('OQ-Consolidate')
@@ -158,7 +174,7 @@ class QConsolidateDialog(QDialog):
                 log_msg(msg, level='C', message_bar=iface.messageBar())
                 self.restoreGui()
                 return
-
+        self.layersDir = outputDir + "/layers"
         # create directory for layers if not exists
         if d.exists("layers"):
             res = QMessageBox.question(
@@ -178,10 +194,10 @@ class QConsolidateDialog(QDialog):
                 return
 
         # copy project file
-        projectFile = QgsProject.instance().fileName()
+        self.projectFile = QgsProject.instance().fileName()
         try:
-            if projectFile:
-                f = QFile(projectFile)
+            if self.projectFile:
+                f = QFile(self.projectFile)
                 newProjectFile = os.path.join(outputDir,
                                               '%s.qgs' % project_name)
                 f.copy(newProjectFile)
@@ -197,22 +213,194 @@ class QConsolidateDialog(QDialog):
                     exception=exc)
             return
 
-        # start consolidate task that does all real work
-        self.consolidateTask = ConsolidateTask(
-            'Consolidation', QgsTask.CanCancel, outputDir, newProjectFile,
-            self.checkBoxZip.isChecked())
-        self.consolidateTask.begun.connect(self.on_consolidation_begun)
-
-        QgsApplication.taskManager().addTask(self.consolidateTask)
+        self.consolidate()
         super().accept()
-
-    def on_consolidation_begun(self):
-        log_msg("Consolidation started.", level='I', duration=4,
-                message_bar=iface.messageBar())
 
     def restoreGui(self):
         self.btnCancel.setEnabled(True)
         self.set_ok_button()
+
+    def consolidate(self):
+        log_msg("Consolidation started.", level='I', duration=4,
+                message_bar=iface.messageBar())
+        gdal.AllRegister()
+
+        # read project
+        doc = self.loadProject()
+        root = doc.documentElement()
+
+        # ensure that relative path used
+        e = root.firstChildElement("properties")
+        (e.firstChildElement("Paths").firstChild()
+            .firstChild().setNodeValue("false"))
+
+        # get layers section in project
+        e = root.firstChildElement("projectlayers")
+
+        # process layers
+        layers = QgsProject.instance().mapLayers()
+        # log_msg("Layers: %s" % layers, level='I',
+        #         message_bar=iface.messageBar())
+
+        # keep full paths of exported layer files (used to zip files)
+        self.outfiles = [self.projectFile]
+        convertable_layers = [
+            layer for layer in layers.values()
+            if (layer.type() == QgsMapLayer.VectorLayer
+                or (layer.type() == QgsMapLayer.RasterLayer
+                and layer.providerType() == 'gdal'))]
+        # msg = 'Convertable layers: %s' % convertable_layers
+        # log_msg(msg, level='I', message_bar=iface.messageBar())
+        self.totExpectedOutFiles = 1 + len(convertable_layers)
+        for i, layer in enumerate(layers.values()):
+            if not layer.isValid():
+                raise TypeError("Layer %s is invalid" % layer.name())
+            lType = layer.type()
+            lProviderType = layer.providerType()
+            lName = layer.name()
+            lUri = layer.dataProvider().dataSourceUri()
+            if lType == QgsMapLayer.VectorLayer:
+                # Always convert to GeoPackage
+                convert_layer_task = QgsTask.fromFunction(
+                    'Converting layer: %s' % layer.name(),
+                    self.convertGenericVectorLayer, e, layer, lName)
+                QgsApplication.taskManager().addTask(convert_layer_task)
+            elif lType == QgsMapLayer.RasterLayer:
+                # FIXME: should we convert also this to GeoPackage?
+                if lProviderType == 'gdal':
+                    if self.checkGdalWms(lUri):
+                        copy_raster_layer_task = QgsTask.fromFunction(
+                            'Copying raster layer: %s' % layer.name(),
+                            self.copyXmlRasterLayer, e, layer, lName)
+                        QgsApplication.taskManager().addTask(
+                            copy_raster_layer_task)
+            else:
+                raise TypeError('Layer %s (type %s) is not supported'
+                                % (lName, lType))
+
+        # save updated project
+        self.saveProject(doc)
+
+        return True
+
+    @pyqtSlot(str)
+    def on_outfileCreated(self, outFile):
+        if not self.checkBoxZip.isChecked():
+            return
+        self.mutex.lock()
+        self.outfiles.append(outFile)
+        self.mutex.unlock()
+        # msg = "%s vs %s" % (len(self.outfiles), self.totExpectedOutFiles)
+        # log_msg(msg, level='I', message_bar=iface.messageBar())
+        if len(self.outfiles) == self.totExpectedOutFiles:
+            # strip .qgs from the project name
+            zip_files_task = QgsTask.fromFunction(
+                'Zipping files', self.zipfiles,
+                self.outfiles, self.projectFile[:-4])
+            QgsApplication.taskManager().addTask(
+                zip_files_task)
+
+    def loadProject(self):
+        f = QFile(self.projectFile)
+        if not f.open(QIODevice.ReadOnly | QIODevice.Text):
+            msg = self.tr("Cannot read file %s:\n%s.") % (self.projectFile,
+                                                          f.errorString())
+            raise IOError(msg)
+
+        doc = QDomDocument()
+        setOk, errorString, errorLine, errorColumn = doc.setContent(f, True)
+        if not setOk:
+            msg = (self.tr("Parse error at line %d, column %d:\n%s")
+                   % (errorLine, errorColumn, errorString))
+            raise SyntaxError(msg)
+
+        f.close()
+        return doc
+
+    def saveProject(self, doc):
+        f = QFile(self.projectFile)
+        if not f.open(QIODevice.WriteOnly | QIODevice.Text):
+            msg = self.tr("Cannot write file %s:\n%s.") % (self.projectFile,
+                                                           f.errorString())
+            raise IOError(msg)
+
+        out = QTextStream(f)
+        doc.save(out, 4)
+        f.close()
+
+    def zipfiles(self, task, file_paths, archive):
+        """
+        Build a zip archive from the given file names.
+        :param file_paths: list of path names
+        :param archive: path of the archive
+        """
+        archive = "%s.zip" % archive
+        prefix = len(
+            os.path.commonprefix([os.path.dirname(f) for f in file_paths]))
+        task.setProgress(1.0)
+        with zipfile.ZipFile(
+                archive, 'w', zipfile.ZIP_DEFLATED, allowZip64=True) as z:
+            for i, f in enumerate(file_paths):
+                task.setProgress(i)
+                z.write(f, f[prefix:])
+
+    def copyXmlRasterLayer(self, task,  layerElement, vLayer, layerName):
+        outFile = "%s/%s.xml" % (self.layersDir, layerName)
+        try:
+            copyfile(vLayer.dataProvider().dataSourceUri(), outFile)
+        except IOError:
+            msg = self.tr("Cannot copy layer %s") % layerName
+            raise IOError(msg)
+
+        # update project
+        layerNode = self.findLayerInProject(layerElement, layerName)
+        tmpNode = layerNode.firstChildElement("datasource")
+        p = "./layers/%s.xml" % layerName
+        tmpNode.firstChild().setNodeValue(p)
+        tmpNode = layerNode.firstChildElement("provider")
+        tmpNode.firstChild().setNodeValue("gdal")
+        self.outfileCreated.emit(outFile)
+
+    def convertGenericVectorLayer(self, task, layerElement, vLayer, layerName):
+        crs = vLayer.crs()
+        enc = vLayer.dataProvider().encoding()
+        outFile = "%s/%s.gpkg" % (self.layersDir, layerName)
+
+        # TODO: If it's already a geopackage, we chould just copy it instead of
+        #       converting it
+        #       (if vLayer.dataProvider().storageType() == 'GPKG':)
+
+        error, error_msg = QgsVectorFileWriter.writeAsVectorFormat(
+            vLayer, outFile, enc, crs, 'GPKG')
+        if error != QgsVectorFileWriter.NoError:
+            msg = self.tr("Cannot copy layer %s: %s") % (layerName, error_msg)
+            raise IOError(msg)
+
+        # update project
+        layerNode = self.findLayerInProject(layerElement, layerName)
+        tmpNode = layerNode.firstChildElement("datasource")
+        p = "./layers/%s.gpkg" % layerName
+        tmpNode.firstChild().setNodeValue(p)
+        tmpNode = layerNode.firstChildElement("provider")
+        tmpNode.setAttribute("encoding", enc)
+        tmpNode.firstChild().setNodeValue("ogr")
+        self.outfileCreated.emit(outFile)
+
+    def findLayerInProject(self, layerElement, layerName):
+        child = layerElement.firstChildElement()
+        while not child.isNull():
+            nm = child.firstChildElement("layername")
+            if nm.text() == layerName:
+                return child
+            child = child.nextSiblingElement()
+        return None
+
+    def checkGdalWms(self, layer):
+        ds = gdal.Open(layer, gdal.GA_ReadOnly)
+        isGdalWms = True if ds.GetDriver().ShortName == "WMS" else False
+        del ds
+
+        return isGdalWms
 
 
 # from https://github.com/django/django/blob/master/django/utils/text.py#L223
